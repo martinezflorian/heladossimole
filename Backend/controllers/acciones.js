e1f51a06496a18d2f48b0dc848c720c1c824bc7f
SamantaRed08 <166084600+SamantaRed08@users.noreply.github.com>
2024-07-03 16:27:50 -0400

    comit 3D julio

--- Backend/controllers/acciones.js
+++ Backend/controllers/acciones.js
@@ -1,8 +1,96 @@
 /* En este archivo tenemos que codificar los métodos
- * .getAllSabores
- * .getSaborID
- * .createPedido
- * .updatePedido
- * .deletePedido
+ * 1 -.getAllSabores /get // acceso admi y usuarios
+ * 2- .editarSabor /put // si por ej, no era solo chocolate sino choco con almendras /acceso solo admi
+ * 3 -.agregarSabor /post //acceso solo admi
+ * 4- .borrarSabor /delete //acceso solo admi
+ * 5- .createPedido /post //acceso usuarios
+ * 6- .elegirSabor /get //acceso usuarios 
+ * 7- .cambiarPedido /put //acceso usuarios y admi
+ * 8- .deletePedido /delete //acceso usuarios y admi
  */
 
+//Importamos el módulo db.js
+// El objeto db posee los métodos para conectar con la base de datos. 
+// Es la conexión a la base de datos. 
+const db = require("../bd/bd.js");
+
+// Generamos el método 1 getAllSabores //get
+const getAllSabores = (req,res)=>{
+    const sql = 'SELECT * FROM sabores';
+    //enviamos consulta a la base de datos
+    bd.query(sql,(err,result)=>{
+        if(err){throw err}
+        res.json(result)
+    });
+};
+
+//Generamos el método 2- editarSabor //put
+const editarSabor =(req,res)=>{
+    const {id}=req.params; 
+    const {name,ingredientes}= req.body;
+    //creamos consulta sql 
+    const sql ='UPDATE sabores SET name=?, ingredientes=? WHERE id =?';
+    //enviar consulta a base de datos
+    bd.query(sql, [name,ingredientes], (err,result)=>{
+        if (err){throw err}
+        res.json({mensaje:"Sabor agregado al menú"})
+    });
+};
+
+
+//Generamos el método 3- agregarSabor //post
+const agregarSabor = (req,res)=>{
+    //desestructuramos request
+    const {name, ingredientes} = req.body 
+    //creamos consulta
+    const sql = 'INSERT INTO sabores (name, ingredientes) VALUES (?,?)';
+    //enviamos a base de datos
+    bd.query(sql, [name,ingredientes], (err,result)=>{
+        if(err){throw err}
+        res.json(result)
+    });
+};
+
+//Gerenamos el método 5- createPedido //post
+const createPedido = (req,res)=>{
+    //desestructuramos req
+    const {tipo, sabores, pago, retiro} =req.body;
+    //creamos consulta
+    const sql = 'INSERT INTO pedidos (tipo, sabores, pago, retiro) VALUES (?,?,?,?)';
+    //enviamos a base de datos
+    bd.query(sql, [tipo, sabores, pago, retiro], (err,result)=>{
+        if(err){throw err}
+        res.json(result) 
+    });
+};
+
+// Generamos el método 6- elegirSabor //get
+const elegirSabor =(req,res)=>{
+    //desestucturación del id
+    const {id} =req.params;
+    //creamos la consulta
+    const sql = 'SELECT * FROM sabores WHERE id = ?';
+    //enviamos a base de datos
+    bd.query(sql, [id], (err,result)=>{
+        if(err) {throw err};
+        res.json(result)
+    });
+};
+
+//Generamos el métoido 7- cambiarPedido //put
+
+//5- .updateMovie
+//const updateMovie = (req,res)=>{
+    //desestructuracion de la cosnulta
+    //const {id} = req.params;
+    //const {title,director,year} = req.body;
+    //creamos la consulta sql 
+    //const sql = 'UPDATE movies SET title = ?, director = ?, year = ? WHERE id = ?';
+    //enviar la consulta a la base de datos
+    //db.query(sql, [title,director,year,id], (err, result)=>{
+        //si sucede algun error
+        //if(err){throw err}
+        //si todo sale bien
+        //res.json({mensaje:"Película actualizada"})
+    //});
+//};
--- Backend/routes/pedidos.js
+++ Backend/routes/pedidos.js
@@ -8,16 +8,22 @@ const router = express.Router();
 //3- importamos el modulo propio
 const acciones = require("../controllers/acciones");
 //4- Declaramos las rutas y métodos, y el llamado específico para cada acción.
-// Ruta de listado en general
+// Ruta de listado en general de sabores
 router.get('/', acciones.getAllSabores);
 //Ruta para la consulta de sabor
-router.get('/:id', acciones.getSaborID);//
+router.put('/:id', acciones.editarSabor);//
+//Ruta para editar el menú de sabores disponibles
+router.post('/', acciones.agregarSabor);
+//Ruta para quitar sabores del menú 
+router.delete('/:id', acciones.borrarSabor);
 //Ruta para crear un pedido
 router.post('/', acciones.createPedido);
+//Ruta para elegir sabores según el pedido
+router.get('/:id', acciones.elegirSabor);
 //Ruta para cambiar pedido
-router.put('/:id', acciones.updatePedido);//
+router.put('/:id', acciones.cambiarPedido);
 //Ruta para borrar pedido
-router.delete('/:id',  acciones.deletePedido);
+router.delete('/:id', acciones.deletePedido);
 //5- Exportamos el módulo
 module.exports = router;
 
